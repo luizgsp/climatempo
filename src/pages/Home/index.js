0e47f3e58c4114f474ca1daffe2c1323cc07f3ca
Luiz Pereira <suportewebsoft@gmail.com>
2021-03-29 12:50:43 +0100

    Created components Header and Menu

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { SafeAreaView, Text, StyleSheet } from 'react-native'
+
+import Menu from '../../components/Menu'
+import Header from '../../components/Header'
 
 export default function Home(){
     return(
-        <View>
-            <Text>Pagina Home</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Menu />
+            <Header />
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#E8F0FF',
+        paddingTop: '5%',
+    }
+})
